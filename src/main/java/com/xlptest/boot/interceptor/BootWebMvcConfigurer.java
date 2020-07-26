bb4f64259711ae36d4a4b6edcc51140d9d53ed33
xuleping <leping.xu@brgroup.com>
2020-07-26 21:52:55 +0800

    [refactor][interrecptor][add march rule]

--- src/main/java/com/xlptest/boot/interceptor/BootWebMvcConfigurer.java
+++ src/main/java/com/xlptest/boot/interceptor/BootWebMvcConfigurer.java
@@ -1,25 +1,40 @@
 package com.xlptest.boot.interceptor;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class BootWebMvcConfigurer implements WebMvcConfigurer {
+    private List<String> addaList = new ArrayList<>();
+    private List<String> excludeList = new ArrayList<>();
+
+    public void init(InterceptorRegistry registry) {
+        addaList.add("/*/*");
+
+        excludeList.add("/user/loginUI");
+        excludeList.add("/user/registerUI");
+
+        registry.addInterceptor(new BootHandlerInterceptor()).addPathPatterns(addaList)
+                .excludePathPatterns(excludeList);
+
+
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new BootHandlerInterceptor()).addPathPatterns("/**")
-                .excludePathPatterns("/user/loginUI")
-                .excludePathPatterns("*.html");
+        this.init(registry);
         System.out.println("addInterceptors");
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         System.out.println("addViewControllers");
+
 //        registry.addViewController("").setViewName("");
     }
    /* @Override
