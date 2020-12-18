61ed1eb70e3512a701b0e74fa10faa5faf3fa92d
Wenjiang Tang <wenjiang.tang@kingsgroupgames.com>
2020-12-18 19:10:52 +0800

    bugfix

--- src/test/java/com/xlptest/boot/Test.java
+++ src/test/java/com/xlptest/boot/Test.java
@@ -5,6 +5,23 @@ package com.xlptest.boot;
  */
 public class Test {
     public static void main(String[] args) {
+        String data ="" ;
 
+        String lan[] = data.split("\n");
+        StringBuilder result= new StringBuilder();
+        for (int i = 0; i < lan.length; i++) {
+            String[] d = lan[i].split(",");
+            String fpid = d[0];
+            String v = d[2];
+               String  r="[\n" +
+                        "        'fpid' => "+fpid+",\n" +
+                        "        'reward_val' => ["+v+"]\n" +
+                        "    ],\n";
+
+            result.append(r);
+
+        }
+
+        System.out.println(result.toString());
     }
 }
