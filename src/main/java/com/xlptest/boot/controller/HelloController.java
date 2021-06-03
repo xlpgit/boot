5a8ec3852b73070d18e87b3698b86e116184b10d
xuleping <leping.xu@brgroup.com>
2021-06-03 16:26:18 +0800

    update annoation

--- src/main/java/com/xlptest/boot/controller/HelloController.java
+++ src/main/java/com/xlptest/boot/controller/HelloController.java
@@ -4,7 +4,6 @@ package com.xlptest.boot.controller;
 import com.xlptest.boot.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
