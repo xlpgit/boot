424108ef446636fe3bd22c1d152023aef0ca552c
xuleping <leping.xu@brgroup.com>
2021-06-03 17:52:51 +0800

    update package

--- src/main/java/com/xlptest/boot/controller/HelloController.java
+++ src/main/java/com/xlptest/boot/controller/HelloController.java
@@ -5,7 +5,6 @@ import com.xlptest.boot.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
