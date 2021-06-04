4014037ff541a0aa4c132f6f7a54d101cd2a935c
xlpgit <1172005694@qq.com>
2021-06-04 15:30:25 +0800

    update package

--- src/main/java/com/xlptest/boot/controller/HelloController.java
+++ src/main/java/com/xlptest/boot/controller/HelloController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
